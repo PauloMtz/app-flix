718a43c0754710ae4d7963b55f79b4ec0f00b631
PauloMtz <pramosmtz@gmail.com>
2022-06-04 21:53:11 -0300

    Listando filmes na página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import api from '../../services/api';
+import './index.css';
 
 // https://api.themoviedb.org/3/movie/now_playing?api_key=47b3ca00a5798ec5cbc9f8707a4ed20a
 
@@ -13,15 +15,30 @@ function Home() {
                     api_key: '47b3ca00a5798ec5cbc9f8707a4ed20a',
                     page: 1
                 }
-            }); console.log(response.data.results);
+            }); 
+            
+            //console.log(response.data.results.slice(0, 10));
+            setFilmes(response.data.results.slice(0, 10));
         }
 
         carregarFilmes();
     }, [])
 
     return (
-        <div>
-            <h1>Página inicial</h1>
+        <div className='container'>
+            <div className='lista-filmes'>
+                {filmes.map((filme) => {
+                    return (
+                        <article key={filme.id}>
+                            <strong>{ filme.title }</strong>
+                            <img 
+                                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                                alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
         </div>
     )
 }
